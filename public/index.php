7a7b163ed19a01a20e8662f920252521d30b0fdd
crysthianophp <crysthianophp@gmail.com>
2015-02-21 00:43:33 -0300

    desenvolviment

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+try {
+
+	$loader = new \Phalcon\Loader();
+	$loader->registerDirs(array(
+		'../app/controllers/',
+		'../app/models/'
+	))->register();
+
+	$di = new Phalcon\DI\FactoryDefault();
+
+	$di->set('view', function() {
+		$view = new \Phalcon\Mvc\View();
+		$view->setViewsDir('../app/views/');
+		return $view;
+	});
+
+	$application = new \Phalcon\Mvc\Application($di);
+
+	echo $application->handle()->getContent();
+
+} catch (\Phalcon\Exception $e) {
+	echo "PhalconException: ", $e->getMessage();
+}
